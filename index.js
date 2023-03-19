34990a9033e0a58e08db5f5a97f083bbb4bdb74e
matthew rendely <matthew.rendely@gmail.com>
2023-03-18 19:55:15 -0700

    done

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  //One letter word
+  if (word.length === 1) return true
+  //Stop index for if even or odd number length word
+  stopIndex = Math.floor(word.length/2)
+  //Loop through
+  for (let i = 0, j=word.length-1; i<= stopIndex; i++, j--){
+    if (i === stopIndex) return true;
+    if (word[i] !== word[j]) return false;
+  }
+
 }
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here:
+
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here:
+  
 */
 
 // You can run `node index.js` to view these console logs
